aee32c806388d46166c32bc4a94a8e06df61e775
ApisitPhysics <apisitphysics@outlook.co.th>
2025-02-08 16:58:13 +0700

    Update setup.py

--- manimae/setup.py
+++ manimae/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="manimae",
     version="1.0.0",
-    description="Evolution of manim library contain a special command to create PPA's education animation",
+    description="Further developed from the original ManimCe.",
     author="Apisit Sengsoon",
     author_email="apisitphysics@outlook.co.th",
     packages=find_packages()
