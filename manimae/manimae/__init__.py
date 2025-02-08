53ced3418fd498b95062b401554e03680c6e34f2
ApisitPhysics <apisitphysics@outlook.co.th>
2025-02-08 13:30:26 +0700

    create LICENSE setup.py and __init__.py

---
+++ manimae/manimae/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from importlib.metadata import version
+
+__version__ = version(__name__)
+
+from manim import *
+
+from math import *
+import numpy as np
---
+++ manimae/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="manimae",
+    version="1.0.0",
+    description="Evolution of manim library contain a special command to create PPA's education animation",
+    author="Apisit Sengsoon",
+    author_email="apisitphysics@outlook.co.th",
+    packages=find_packages()
+)
